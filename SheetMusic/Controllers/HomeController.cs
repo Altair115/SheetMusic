659565180c21d30d8569881642611dd7e4fe1791
Wouter <woutervdixhoorn@gmail.com>
2019-04-05 13:34:18 +0200

    Login pagina veranderd
    Mooi!!

--- SheetMusic/Controllers/HomeController.cs
+++ SheetMusic/Controllers/HomeController.cs
@@ -10,19 +10,11 @@ namespace SheetMusic.Controllers
     {
         public ActionResult Index()
         {
-            return View();
-        }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
+            if (!Request.IsAuthenticated)
+            {
+                return RedirectToAction("LogIn", "Account");
+            }
 
             return View();
         }
