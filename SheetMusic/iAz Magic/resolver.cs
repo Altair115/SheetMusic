d9f06b0d33df68844070a82811f8db6b4066efe1
Thomas <thomaspap@outlook.com>
2019-05-01 10:59:05 +0200

    Search engine Fix

--- SheetMusic/iAz Magic/resolver.cs
+++ SheetMusic/iAz Magic/resolver.cs
@@ -8,23 +8,20 @@ namespace SheetMusic
     public class resolver
     {
 
-
-        private string _filter = "name";
-
         public static string getQueryString(string param)
         {
             switch (param)
             {
                 case "name":
-                    return "SELECT * FROM Pieces WHERE LOWER(PieceName) LIKE LOWER(%@0%)";
+                    return "SELECT * FROM Pieces WHERE PieceName LIKE LOWER(@0)";
                 case "genre":
-                    return "SELECT * FROM Pieces WHERE LOWER(Genre) LIKE LOWER(%@0%)";
+                    return "SELECT * FROM Pieces WHERE Genre LIKE LOWER(@0)";
                 case "year":
-                    return "SELECT * FROM Pieces WHERE LOWER(Year) LIKE LOWER(%@0%)";
+                    return "SELECT * FROM Pieces WHERE Year LIKE LOWER(@0)";
                 case "difficulty":
-                    return "SELECT * FROM Pieces WHERE LOWER(Difficulty) LIKE LOWER(%@0%)";
+                    return "SELECT * FROM Pieces WHERE Difficulty LIKE LOWER(@0)";
                 default:
-                    return "SELECT * FROM Pieces WHERE LOWER(PieceName) LIKE LOWER(%@0%)";
+                    return "SELECT * FROM Pieces WHERE PieceName LIKE LOWER(@0)";
             }
         }
 
